1328a0055a9df1535e891cd07e5e948d298b9ea5
Michal Rakowski <kontakt.rakowski@gmail.com>
2020-09-15 18:50:36 +0200

    Models and migrations

--- inkind/models.py
+++ inkind/models.py
@@ -1,3 +1,45 @@
-from django.db import models
+git from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Institution(models.Model):
+
+    class InstitutionType(models.TextChoices):
+        FUNDACJA = 'FUND', _('Fundacja')
+        ORGANIZACJA_POZARZADOWA = 'OPOZ', _('Organizacja pozarządowa')
+        ZBIORKA_LOCALNA = 'ZLOK', _('Zbiórka lokalna')
+
+    institution_type = models.CharField(max_length=4, 
+        choices=InstitutionType.choices, default=InstitutionType.FUNDACJA)
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+
+class Donation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True) 
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+    
+    quantity = models.PositiveSmallIntegerField(verbose_name="Liczba worków")
+    address = models.CharField(max_length=150, verbose_name="Ulica i numer domu")
+    phone_number = models.CharField(max_length=25, verbose_name="Numer telefonu")
+    city = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=15)
+    pick_up_date = models.DateField()
+    pick_up_time = models.DateTimeField()
+    pick_up_comment = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.user.username}({self.city}) Donation'
+
